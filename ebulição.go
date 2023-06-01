d39c94797736584e4bc1af42f50d59872a246948
M4.unknow <54968908+m4rco0@users.noreply.github.com>
2023-06-01 20:55:26 -0300

    converter temperatura

--- ebulição.go
+++ ebulição.go
@@ -9,8 +9,7 @@ const ebuK float64 = 373.2
 // C = K -237
 func main() {
 	tempK := ebuK
-	tempC := int(tempK - 273)
-	fmt.Printf("temperatura em K = %g temperatura em C = %v", tempK, tempC)
+	tempC := tempK - 273.0
+	fmt.Printf("temperatura em K = %g temperatura em C = %g", tempK, tempC)
 
 }
-
